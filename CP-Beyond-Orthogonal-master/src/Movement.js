c1677774bd6da6029bf59fb0ff047f17c8185e8f
peilin16 <105468929+peilin16@users.noreply.github.com>
2025-01-24 17:59:36 -0800

    Add files via upload

--- CP-Beyond-Orthogonal-master/src/Movement.js
+++ CP-Beyond-Orthogonal-master/src/Movement.js
@@ -6,14 +6,16 @@ class Movement extends Phaser.Scene {
         this.PLAYER_VELOCITY = 350;
     }
     preload(){
-        this.load.spritesheet('character','./assets/spritesheets/Character_002.png',{frameWidth:40})
+        this.load.spritesheet('character','./assets/spritesheets/Character_002.png',{
+            frameWidth:48
+        })
     }
     create(){
-        this.cameras.main.setBackgroundColor(0xDDDDDDD);
-        this.player = this.add.sprite(width / 2, height / 2,'character',1).setScale(2)
+        this.cameras.main.setBackgroundColor(0xDDDDDD);
+        this.player = this.physics.add.sprite(width / 2, height / 2,'character',1).setScale(2)
         
-        this.player.body.setCollideWorldBouunds(true)
-        this.player.body.setSize(32,32).setOffset(8,16);
+        this.player.body.setCollideWorldBounds(true)
+        this.player.body.setSize(32,32).setOffset(8,16)
 
 
         this.anims.create({
@@ -21,8 +23,8 @@ class Movement extends Phaser.Scene {
             frameRate:0,
             repeat:-1,
             frames:this.anims.generateFrameNumbers('character',{
-                start:0,
-                end:2
+                start:1,
+                end:1
             })
         })
         this.anims.create({
@@ -32,6 +34,15 @@ class Movement extends Phaser.Scene {
             frames:this.anims.generateFrameNumbers('character',{
                 start:9,
                 end:11
+            })
+        })
+        this.anims.create({
+            key:'walk-down',
+            frameRate:5,
+            repeat:-1,
+            frames:this.anims.generateFrameNumbers('character',{
+                start:0,
+                end:2
             })
         })
         this.anims.create({
@@ -49,21 +60,21 @@ class Movement extends Phaser.Scene {
 
     }
     update(){
-        let playerVector =new phaser.Math.Vector2(0,0)
+        let playerVector =new Phaser.Math.Vector2(0,0)
         let playerDirection = 'down'
         if(cursors.left.isDown){
-            this.player.x -= this.PLAYER_VELOCITY;
+            this.player.x -= 2;
             playerDirection = 'left'
         }else if(cursors.right.isDown){
-            this.player.x += this.PLAYER_VELOCITY
+            this.player.x += 2
              playerDirection = 'right'
         }
 
         if(cursors.up.isDown){
-            this.player.y -= -1
+            this.player.y -= 2
              playerDirection = 'up'
         }else if(cursors.down.isDown){
-            this.player.y -= 1
+            this.player.y += 2
              playerDirection = 'down'
         }
         playerVector.normalize()
@@ -71,10 +82,11 @@ class Movement extends Phaser.Scene {
        // this.player.y += 
 
 
-       this.player.setVelocity(this.PLAYER_VELOCITY. playerVector.x,this.PLAYER_VELOCITY * playerVector.y)
+       this.player.setVelocity(this.PLAYER_VELOCITY * playerVector.x,this.PLAYER_VELOCITY * playerVector.y)
         let playerMovement 
         playerVector.length() ? playerMovement = 'walk':
         playerMovement = 'idle'
         this.player.play(playerMovement + '-'+ playerDirection,true)
+        
     }
 }
